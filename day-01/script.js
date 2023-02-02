46c759a8829c8b891297c132e7557e433bbf36e9
ferniemann <hello@ferdinand-niemann.eu>
2023-02-02 23:40:30 +0100

    renaming

---
+++ day-01/script.js
@@ -0,0 +1,9 @@
+console.log("Hello World"); // String
+console.log("20"); // String
+console.log("Ich bin bei 'Coding Bootcamps Europe'");
+console.log(20 + "5"); // Number
+console.log(true); // Boolean
+console.log(undefined); // undefined
+console.log(null); // null
+
+console.log({ age: 20 });
